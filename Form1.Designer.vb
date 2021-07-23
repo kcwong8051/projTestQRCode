4dce2e6e1df47eed66b6c432683980d69c5ac1f3
wong ka chun <kachun@prizemart.com>
2021-07-23 17:06:08 +0800

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,108 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtText = New System.Windows.Forms.TextBox()
+        Me.cmdGenerate = New System.Windows.Forms.Button()
+        Me.pbQrCodeImg = New System.Windows.Forms.PictureBox()
+        Me.lblBarcode = New System.Windows.Forms.Label()
+        CType(Me.pbQrCodeImg, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Left), System.Windows.Forms.AnchorStyles)
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("PMingLiU", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.Label1.Location = New System.Drawing.Point(5, 430)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(47, 19)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Text:"
+        '
+        'txtText
+        '
+        Me.txtText.Anchor = System.Windows.Forms.AnchorStyles.Bottom
+        Me.txtText.Font = New System.Drawing.Font("PMingLiU", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.txtText.Location = New System.Drawing.Point(55, 427)
+        Me.txtText.Name = "txtText"
+        Me.txtText.Size = New System.Drawing.Size(177, 30)
+        Me.txtText.TabIndex = 1
+        '
+        'cmdGenerate
+        '
+        Me.cmdGenerate.Anchor = CType((System.Windows.Forms.AnchorStyles.Bottom Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.cmdGenerate.Font = New System.Drawing.Font("PMingLiU", 14.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(136, Byte))
+        Me.cmdGenerate.Location = New System.Drawing.Point(335, 425)
+        Me.cmdGenerate.Name = "cmdGenerate"
+        Me.cmdGenerate.Size = New System.Drawing.Size(92, 32)
+        Me.cmdGenerate.TabIndex = 3
+        Me.cmdGenerate.Text = "QR Code"
+        Me.cmdGenerate.UseVisualStyleBackColor = True
+        '
+        'pbQrCodeImg
+        '
+        Me.pbQrCodeImg.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.pbQrCodeImg.Location = New System.Drawing.Point(92, 43)
+        Me.pbQrCodeImg.Name = "pbQrCodeImg"
+        Me.pbQrCodeImg.Size = New System.Drawing.Size(250, 250)
+        Me.pbQrCodeImg.TabIndex = 4
+        Me.pbQrCodeImg.TabStop = False
+        '
+        'lblBarcode
+        '
+        Me.lblBarcode.AutoSize = True
+        Me.lblBarcode.Font = New System.Drawing.Font("PMingLiU", 14.0!)
+        Me.lblBarcode.Location = New System.Drawing.Point(168, 306)
+        Me.lblBarcode.Name = "lblBarcode"
+        Me.lblBarcode.Size = New System.Drawing.Size(99, 19)
+        Me.lblBarcode.TabIndex = 5
+        Me.lblBarcode.Text = "0123456789"
+        Me.lblBarcode.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(434, 461)
+        Me.Controls.Add(Me.lblBarcode)
+        Me.Controls.Add(Me.pbQrCodeImg)
+        Me.Controls.Add(Me.cmdGenerate)
+        Me.Controls.Add(Me.txtText)
+        Me.Controls.Add(Me.Label1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.pbQrCodeImg, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtText As TextBox
+    Friend WithEvents cmdGenerate As Button
+    Friend WithEvents pbQrCodeImg As PictureBox
+    Friend WithEvents lblBarcode As Label
+End Class
---
+++ Form1.vb
@@ -0,0 +1,35 @@
+﻿Imports MessagingToolkit.QRCode.Codec
+'Imports MessagingToolkit.QRCode.Codec.Data
+'Imports System.IO
+
+Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.Text = "QR Code Reader Generator"
+    End Sub
+
+    Private Sub cmdGenerate_Click(sender As Object, e As EventArgs) Handles cmdGenerate.Click
+        'Dim Generator As New MessagingToolkit.Barcode.BarcodeEncoder
+        ''Generator.BackColor = Color.White
+        'Generator.LabelFont = New Font("Arial", 7, FontStyle.Regular)
+        'Generator.IncludeLabel = True
+        'Generator.CustomLabel = txtText.Text
+        'Try
+        '    pbQrCodeImg.Image = New Bitmap(Generator.Encode(BarcodeFormat.QRCode, txtText.Text, Nothing))
+        'Catch ex As Exception
+        '    pbQrCodeImg.Image = Nothing
+        'End Try
+    End Sub
+
+    Private Sub setQrCode()
+        pbQrCodeImg.SizeMode = PictureBoxSizeMode.StretchImage
+        Dim i_barcode As String = txtText.Text
+        Dim encoder As QRCodeEncoder = New QRCodeEncoder
+        Dim qrcode As Bitmap = encoder.Encode(i_barcode)
+        pbQrCodeImg.Image = qrcode
+    End Sub
+
+    Private Sub txtText_TextChanged(sender As Object, e As EventArgs) Handles txtText.TextChanged
+        lblBarcode.Text = txtText.Text
+        setQrCode()
+    End Sub
+End Class
